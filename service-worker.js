e75a544ba6e3e3e7a9ee2d9fb3f3b7547572b4d4
vitalica <50740482+vitalica74@users.noreply.github.com>
2026-08-10 11:33:04 +0300

    Cache v4.1 UI cycle renderer

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='iching-pwa-v21';
-const ASSETS=['./','./index.html','./css/styles.css','./js/app.js','./js/random.js','./js/storage.js','./js/ui.js','./js/interpretation.js','./js/preferences.js','./data/hexagrams.js','./data/hexagram-cycles-01-10.js','./data/changing-lines.js','./data/changing-lines-01-04.js','./data/changing-lines-05-10.js','./data/transitions.js','./data/profiles.js','./data/classical-sources.js','./data/classical-lines.js','./data/classical-lines-v41.js','./data/classical-lines-01-04.js','./data/classical-lines-05-10.js','./manifest.webmanifest','./icons/icon.svg'];
+const CACHE='iching-pwa-v22';
+const ASSETS=['./','./index.html','./css/styles.css','./js/app.js','./js/v41-ui.js','./js/random.js','./js/storage.js','./js/ui.js','./js/interpretation.js','./js/preferences.js','./data/hexagrams.js','./data/hexagram-cycles-01-10.js','./data/changing-lines.js','./data/changing-lines-01-04.js','./data/changing-lines-05-10.js','./data/transitions.js','./data/profiles.js','./data/classical-sources.js','./data/classical-lines.js','./data/classical-lines-v41.js','./data/classical-lines-01-04.js','./data/classical-lines-05-10.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
