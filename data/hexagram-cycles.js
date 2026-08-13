dfa07abd580903284b3509e9217744bc9a004ca8
vitalica <50740482+vitalica74@users.noreply.github.com>
2026-08-13 23:38:00 +0300

    Fix unified cycle registry

--- data/hexagram-cycles.js
+++ data/hexagram-cycles.js
@@ -1,7 +1,6 @@
-import {HEXAGRAM_CYCLES_01_10} from './hexagram-cycles-01-10.js';
-import {HEXAGRAM_CYCLES_11_16} from './hexagram-cycles-11-16-data.js';
+import {HEXAGRAM_CYCLES_01_10} from './hexagram-cycles-01-10-data.js';
+import {HEXAGRAM_CYCLES_11_16_DATA} from './hexagram-cycles-11-16-data.js';
 import {HEXAGRAM_CYCLES_17_24} from './hexagram-cycles-17-24.js';
 
-const CYCLES={...HEXAGRAM_CYCLES_01_10,...HEXAGRAM_CYCLES_11_16,...HEXAGRAM_CYCLES_17_24};
-
+const CYCLES={...HEXAGRAM_CYCLES_01_10,...HEXAGRAM_CYCLES_11_16_DATA,...HEXAGRAM_CYCLES_17_24};
 export function getHexagramCycle(number){return CYCLES[Number(number)]||null;}
