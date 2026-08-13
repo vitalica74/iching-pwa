e03fafda796f59eac9bdee8836173fa214da34dd
vitalica <50740482+vitalica74@users.noreply.github.com>
2026-08-14 01:51:14 +0300

    Connect cycles through hexagram 32

--- data/hexagram-cycles.js
+++ data/hexagram-cycles.js
@@ -1,6 +1,7 @@
 import {HEXAGRAM_CYCLES_01_10} from './hexagram-cycles-01-10-data.js';
 import {HEXAGRAM_CYCLES_11_16_DATA} from './hexagram-cycles-11-16-data.js';
 import {HEXAGRAM_CYCLES_17_24} from './hexagram-cycles-17-24.js';
+import {HEXAGRAM_CYCLES_25_32} from './hexagram-cycles-25-32.js';
 
-const CYCLES={...HEXAGRAM_CYCLES_01_10,...HEXAGRAM_CYCLES_11_16_DATA,...HEXAGRAM_CYCLES_17_24};
+const CYCLES={...HEXAGRAM_CYCLES_01_10,...HEXAGRAM_CYCLES_11_16_DATA,...HEXAGRAM_CYCLES_17_24,...HEXAGRAM_CYCLES_25_32};
 export function getHexagramCycle(number){return CYCLES[Number(number)]||null;}
