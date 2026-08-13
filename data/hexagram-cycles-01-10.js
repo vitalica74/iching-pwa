dcdededb86ea35852e2c39739733e2e3f3573968
vitalica <50740482+vitalica74@users.noreply.github.com>
2026-08-14 02:56:03 +0300

    Activate cycles through hexagram 64

--- data/hexagram-cycles-01-10.js
+++ data/hexagram-cycles-01-10.js
@@ -1,2 +1,2 @@
 export {HEXAGRAM_CYCLES_01_10} from './hexagram-cycles-01-10-data.js';
-export {getHexagramCycle} from './hexagram-cycles-49-56-registry.js';
+export {getHexagramCycle} from './hexagram-cycles-57-64-registry.js';
