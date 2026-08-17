5e4955566170442a9fc0c00c044d172acf5fc5d7
vitalica <50740482+vitalica74@users.noreply.github.com>
2026-08-17 16:35:42 +0300

    Wire crossroads for hexagrams 17-24

--- data/crossroads.js
+++ data/crossroads.js
@@ -1,8 +1,9 @@
 import {CROSSROADS_01_04} from './crossroads-01-04.js';
 import {CROSSROADS_05_10} from './crossroads-05-10.js';
 import {CROSSROADS_11_16} from './crossroads-11-16.js';
+import {CROSSROADS_17_24} from './crossroads-17-24.js';
 
-const CROSSROADS={...CROSSROADS_01_04,...CROSSROADS_05_10,...CROSSROADS_11_16};
+const CROSSROADS={...CROSSROADS_01_04,...CROSSROADS_05_10,...CROSSROADS_11_16,...CROSSROADS_17_24};
 export const CROSSROADS_PRINCIPLE='Це лише частина шляхів, які видно звідси. Можна побачити інший і створити власний.';
 
 const unique=items=>[...new Set(items.filter(Boolean))];
