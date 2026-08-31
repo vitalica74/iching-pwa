f30ee57d5cb32778f5efcf260ba052a2177ae968
vitalica <50740482+vitalica74@users.noreply.github.com>
2026-08-31 14:49:41 +0300

    Bump app version to 1.0.16

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 // Єдине джерело версії додатка.
 // Для нового релізу змінюйте лише це значення.
-export const VERSION='1.0.15';
+export const VERSION='1.0.16';
 export const APP_VERSION=VERSION;
 export const LIBRARY_VERSION=VERSION;
 export const ENGINE_VERSION=VERSION;
